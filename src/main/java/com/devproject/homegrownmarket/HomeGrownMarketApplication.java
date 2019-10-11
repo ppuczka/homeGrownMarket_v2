29ea739ee2263ae0585112ae727c5741818eb0c8
Przemyslaw Puczka <p.puczka@gmail.com>
2019-10-11 23:02:14 +0200

    homegrownmarket

---
+++ src/main/java/com/devproject/homegrownmarket/HomeGrownMarketApplication.java
@@ -0,0 +1,22 @@
+package com.devproject.homegrownmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HomeGrownMarketApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeGrownMarketApplication.class, args);
+    }
+
+}
