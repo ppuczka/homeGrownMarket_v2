500fc6957e4b409e8f9c75c0b2bf6b819d759d34
Przemyslaw Puczka <p.puczka@gmail.com>
2019-10-23 21:30:42 +0200

    homegrownmarket user entity

--- src/main/java/com/devproject/homegrownmarket/entity/User.java
+++ src/main/java/com/devproject/homegrownmarket/entity/User.java
@@ -1,4 +1,27 @@
 package com.devproject.homegrownmarket.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    
+    @Column
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    
+    @Column
+    private String firstName;
+    
+    @Column
+    private String lastName; 
+    
+    @Column
+    private String userName; 
+    
+    @Column
+    private String password; 
 }
