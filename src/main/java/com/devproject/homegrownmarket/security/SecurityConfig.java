f1f01f6f15e99cff97dcbb42a8487969e9cba7a5
Przemyslaw Puczka <p.puczka@gmail.com>
2019-10-23 22:06:53 +0200

    basic spring security for REST implementation

---
+++ src/main/java/com/devproject/homegrownmarket/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.devproject.homegrownmarket.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userSearchBase("ou=people")
+                .userSearchFilter("(uid={0})")
+                .groupSearchBase("ou=groups")
+                .groupSearchFilter("(member={0})")
+                .contextSource().root("dc=baeldung,dc=com")
+                .ldif("classpath:users.ldif");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated();
+        http.formLogin().loginPage("/login").permitAll().and().logout().logoutSuccessUrl("/");
+    }
+}
